e29d66ca0ad43ba324d7c51f33e8d3e03bf3fb1e
sos1819ajsm <antsanmun1@alum.us.es>
2019-04-02 20:17:38 +0000

    Update API Antonio J

--- generalPublicExpensesAPI/index.js
+++ generalPublicExpensesAPI/index.js
@@ -489,7 +489,7 @@ apiRest.register = (app, generalPublicExpenses) => {
                         //Miramos si existe algún error (ej: solicitud malformada, sintaxis errónea, etc)
                         if (updateData.country != req.params.country || updateData.year != params["year"] || !isNaN(updateData.country) ||
                             isNaN(updateData.year) || isNaN(updateData.publicSpending) || isNaN(updateData.educationExpense) ||
-                            isNaN(updateData.healtExpense) || isNaN(updateData.defenseSpending) || isNaN(updateData.publicSpendingPib) ||
+                            isNaN(updateData.healthExpense) || isNaN(updateData.defenseSpending) || isNaN(updateData.publicSpendingPib) ||
                             isNaN(updateData.var_) ||
                             !updateData.hasOwnProperty("country") || !updateData.hasOwnProperty("year") || !updateData.hasOwnProperty("publicSpending") ||
                             !updateData.hasOwnProperty("educationExpense") || !updateData.hasOwnProperty("healthExpense") ||

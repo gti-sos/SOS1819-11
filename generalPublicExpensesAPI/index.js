7ff1b9400444019c460ddd7c42bf1ebab41f2b71
sos1819ajsm <antsanmun1@alum.us.es>
2019-04-02 18:55:28 +0000

    Update initial data Antonio J

--- generalPublicExpensesAPI/index.js
+++ generalPublicExpensesAPI/index.js
@@ -17,57 +17,57 @@ apiRest.register = (app, generalPublicExpenses) => {
             var newGeneralPublicExpenses = [{
 
                     country: "espania",
-                    year: "2017",
-                    publicSpending: "478126,0",
-                    educationExpense: "9,77",
-                    healthExpense: "15,14",
-                    defenseSpending: "2,99",
-                    publicSpendingPib: "41,00",
-                    var_: "-1,20"
+                    year: 2017,
+                    publicSpending: 478126.0,
+                    educationExpense: 9.77,
+                    healthExpense: 15.14,
+                    defenseSpending: 2.99,
+                    publicSpendingPib: 41.00,
+                    var_: -1.20
 
                 },
                 {
                     country: "alemania",
-                    year: "2017",
-                    publicSpending: "1439839,0",
-                    educationExpense: "10,98",
-                    healthExpense: "21,36",
-                    defenseSpending: "2,73",
-                    publicSpendingPib: "43,90",
-                    _var: "0"
+                    year: 2017,
+                    publicSpending: 1439839.0,
+                    educationExpense: 10.98,
+                    healthExpense: 21.36,
+                    defenseSpending: 2.73,
+                    publicSpendingPib: 43.90,
+                    _var: 0
 
                 },
                 {
                     country: "francia",
-                    year: "2017",
-                    publicSpending: "1.291.948,0",
-                    educationExpense: "9,66",
-                    healthExpense: "16,97",
-                    defenseSpending: "4,01",
-                    publicSpendingPib: "56,50",
-                    var_: "-0,10"
+                    year: 2017,
+                    publicSpending: 1291948.0,
+                    educationExpense: 9.66,
+                    healthExpense: 16.97,
+                    defenseSpending: 4.01,
+                    publicSpendingPib: 56.50,
+                    var_: -0.10
 
                 },
                 {
                     country: "italia",
-                    year: "2017",
-                    publicSpending: "840.763,0",
-                    educationExpense: "8,11",
-                    healthExpense: "13,47",
-                    defenseSpending: "3,12",
-                    publicSpendingPib: "48,70",
-                    var_: "-0,40"
+                    year: 2017,
+                    publicSpending: 840763.0,
+                    educationExpense: 8.11,
+                    healthExpense: 13.47,
+                    defenseSpending: 3.12,
+                    publicSpendingPib: 48.70,
+                    var_: -0.40
 
                 },
                 {
                     country: "reino unido",
-                    year: "2017",
-                    publicSpending: "954262,1",
-                    educationExpense: "13,91",
-                    healthExpense: "18,88",
-                    defenseSpending: "4,66",
-                    publicSpendingPib: "40,90",
-                    var_: "-0,40"
+                    year: 2017,
+                    publicSpending: 954262.1,
+                    educationExpense: 13.91,
+                    healthExpense: 18.88,
+                    defenseSpending: 4.66,
+                    publicSpendingPib: 40.90,
+                    var_: -0.40
 
                 }
             ];

4600d6aa1a2b8740caface1f4664b1c9ba54c713
sos1819jmc <joamocap@gmail.com>
2019-04-09 19:09:00 +0000

    added controller

---
+++ public/public-health-expenses/controllers/minipostman-ctrl.js
@@ -0,0 +1,61 @@
+/* global angular */
+var app = angular.module("MiniPostmanApp");
+
+app.controller("MainCtrl", ["$scope", "$http", function($scope, $http) {
+
+    console.log("MainCtrl initialized!");
+
+    $scope.url = "/api/v1/public-health-expenses";
+
+    $scope.send = {
+        country: "france",
+        year: 2017,
+        publicHealthExpense: 82.99,
+        healthExpense: 217528.3,
+        totalPublicExpense: 16.97,
+        healthExpensePib: 9.51,
+        healthExpenditurePerCapita: 3247,
+        var_: 3.80
+    };
+
+    $scope.get = function() {
+        $http.get($scope.url).then(function(response) {
+            $scope.state = JSON.stringify(response.status, null, 2);
+            $scope.data = JSON.stringify(response.data, null, 2);
+        }).catch(function(response) {
+            $scope.state = JSON.stringify(response.status, null, 2);
+            $scope.data = JSON.stringify(response.data, null, 2);
+        });
+    };
+
+    $scope.delete = function() {
+        $http.delete($scope.url).then(function(response) {
+            $scope.state = JSON.stringify(response.status, null, 2);
+            $scope.data = JSON.stringify(response.data, null, 2);
+        }).catch(function(response) {
+            $scope.state = JSON.stringify(response.status, null, 2);
+            $scope.data = JSON.stringify(response.data, null, 2);
+        });
+    };
+
+    $scope.post = function(send) {
+        $http.post($scope.url, $scope.send).then(function(response) {
+            $scope.state = JSON.stringify(response.status, null, 2);
+            $scope.data = JSON.stringify(response.data, null, 2);
+        }).catch(function(response) {
+            $scope.state = JSON.stringify(response.status, null, 2);
+            $scope.data = JSON.stringify(response.data, null, 2);
+        });
+    };
+
+    $scope.put = function(send) {
+        $http.put($scope.url, $scope.send).then(function(response) {
+            $scope.state = JSON.stringify(response.status, null, 2);
+            $scope.data = JSON.stringify(response.data, null, 2);
+        }).catch(function(response) {
+            $scope.state = JSON.stringify(response.status, null, 2);
+            $scope.data = JSON.stringify(response.data, null, 2);
+        });
+    };
+
+}]);

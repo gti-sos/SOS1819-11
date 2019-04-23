dd719a1e74748f2e5458af8ccd7cf92920446fc7
sos1819jmcc <jmcentenocaballero@gmail.com>
2019-04-23 18:51:04 +0000

    Front End (ngRoute v0.1.1.0.2)

--- public/public-expenditure-educations/app.js
+++ public/public-expenditure-educations/app.js
@@ -3,13 +3,21 @@
 angular.module("App", [ "ngRoute", "ngDialog"]).config(function($routeProvider,$locationProvider) {
     
     
+    /* for delete /#!*/
+    
+    
+    $locationProvider.html5Mode({
+       enabled: true,
+       requireBase: false
+    });
+    
     
     $routeProvider.
-    when("/minipostman/", {
+    when("/minipostman", {
         templateUrl: "/public-expenditure-educations/views/miniPostman.html",
         controller: "MainCtrl"
     }).
-    when("/ui/v1/public-expenditure-educations/", {
+    when("/ui/v1/public-expenditure-educations", {
         templateUrl: "/public-expenditure-educations/views/educationsList.html",
         controller: "EducatiosListCtrl"
        
@@ -18,8 +26,7 @@ angular.module("App", [ "ngRoute", "ngDialog"]).config(function($routeProvider,$
         templateUrl: "/public-expenditure-educations/views/educationsEdit.html",
         controller: "EducatiosEditCtrl"
        
-    }).when('/', {redirectTo: "/ui/v1/public-expenditure-educations/"});;
-    
+    });
 });
 
 console.log("App initialized!");

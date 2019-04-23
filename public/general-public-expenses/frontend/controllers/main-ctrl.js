1b1aeac5747fe8e94dc035eb4a2d3e172ec8e72b
sos1819ajsm <antsanmun1@alum.us.es>
2019-04-23 20:16:18 +0000

    First update api front-end AJ

---
+++ public/general-public-expenses/frontend/controllers/main-ctrl.js
@@ -0,0 +1,120 @@
+/* global angular */
+var app = angular.module("GeneralPublicExpensesApp");
+
+app.controller("MainCtrl", ["$scope", "$http", function ($scope,$http){
+                console.log("MainCtrl initialized");
+                $scope.url = "https://sos1819-11.herokuapp.com/api/v1/general-public-expenses";
+                
+                refresh();
+                
+                function refresh() {
+                    console.log("Requesting earnings inter stats to <"+$scope.url+">...");
+                    $http.get($scope.url).then(function (response){
+                        console.log("Data received: " + JSON.stringify(response.data,null,2));
+                        $scope.generalPublicExpenses = response.data;
+                    });
+                }
+
+                $scope.loadInitialData = function (){
+                        $http.get($scope.url + "/" + "loadInitialData").then(function (response){
+                            alert("Base de datos inicializada.");
+                            $scope.data = JSON.stringify(response.data,null,2);
+                            $scope.statusInfo = JSON.stringify(response.status, null, 2);
+                        }).catch(function (response) {
+			            	$scope.statusInfo = JSON.stringify(response.status, null, 2);
+			            });
+			            refresh();
+                };
+                
+                $scope.addGeneralPublicExpenses = function() {
+                    var newGeneralPublicExpenses = $scope.newGeneralPublicExpenses;
+                    console.log("Adding a new earning inter stat: " + JSON.stringify(newGeneralPublicExpenses,null,2)); 
+                    $http.post($scope.url,newGeneralPublicExpenses).then(function (response){
+                        alert("El dato  " + JSON.stringify(newGeneralPublicExpenses.title,null,2) + " se ha añadido correctamente.");
+                        console.log("POST Response: " + response.status + " " + response.data);
+                        refresh();
+                    }, function (error){
+                        if(error.status==400) {
+                            alert("Error: Debe rellenar todos los campos.");
+                        }
+                        else if(error.status==409) {
+                            alert("Error: El dato " + JSON.stringify(newGeneralPublicExpenses.title,null,2) + " ya existe.");
+                        }
+                    });
+                };
+
+                $scope.deleteGeneralPublicExpenses = function(title) {
+                    console.log("Deleting general public expenses with title: " + title); 
+                    $http.delete($scope.url + "/" + title).then(function (response){
+                        alert("El dato se ha eliminado correctamente.");
+                        console.log("DELETE Response: " + response.status + " " + response.data);
+                        refresh();
+                    });
+                };
+                
+                $scope.deleteAll = function(){
+                    console.log("Deleting all general public expenses");
+                    $http.delete($scope.url).then(function(response){
+                        alert("Todas los datos se han eliminado correctamente.");
+                        $scope.data = "";
+                        console.log("DELETE ALL Response: " + response.status + " " + response.data);
+                        refresh();
+                    });
+                };
+                
+                $scope.busqueda = function() {
+                    console.log($scope.url + "?" + $scope.atributo + "=" + $scope.valor);
+                    $http.get($scope.url + "?" + $scope.atributo + "=" + $scope.valor).then(function(response) {
+                        if(response.data.length == 0) {
+                           alert("No se ha encontrado ninguna dato.");
+                        }else if(response.data.length == 1){
+                            alert("Se ha encontrado 1 dato.");
+                        }else{
+                            alert("Se han encontrado " + response.data.length + " dato.");
+                        }
+                        $scope.generalPublicExpenses = response.data;
+                        console.log("Búsqueda Response: " + response.status + " " + JSON.stringify(response.data,null,2));
+                    });
+                };
+                
+                $scope.paginacion = function() {
+                    console.log($scope.url + "?limit=" + $scope.limit + "&offset=" + $scope.offset);
+                    $http.get($scope.url + "?limit=" + $scope.limit + "&offset=" + $scope.offset).then(function(response) {
+                        alert("Paginación realizada correctamente.");
+                        $scope.generalPublicExpenses = response.data;
+                        console.log("Paginación Response: " + response.status + " " + JSON.stringify(response.data,null,2));
+                    });
+                };
+                
+                $scope.paginaAnterior = function() {
+                    $scope.offset = $scope.offset - $scope.limit;
+                    $http.get($scope.url + "?limit=" + $scope.limit + "&offset=" + $scope.offset).then(function(response) {
+                        $scope.generalPublicExpenses = response.data;
+                    });
+                };
+        
+                $scope.paginaSiguiente = function() {
+                    $scope.offset = $scope.offset + $scope.limit;
+                    $http.get($scope.url + "?limit=" + $scope.limit + "&offset=" + $scope.offset).then(function(response) {
+                        $scope.generalPublicExpenses = response.data;
+                        $scope.error = "";
+                    });
+                };
+                
+                $scope.updateGeneralPublicExpenses = function(){
+                    var newGeneralPublicExpenses = $scope.newGeneralPublicExpenses;
+                    console.log("Updating general public expenses: " + JSON.stringify(newGeneralPublicExpenses, null, 2));
+                    $http.put($scope.url+"/"+newGeneralPublicExpenses.title, newGeneralPublicExpenses).then(function(response) {
+                        alert("El dato " + JSON.stringify(newGeneralPublicExpenses.title,null,2) + " se ha modificado correctamente.");
+                        console.log("PUT Response: " + response.status + " " + response.statusText);
+                        refresh();
+                    }, function (error){
+                        if(error.status==400) {
+                            alert("Error: Debe rellenar todos los campos.");
+                        }
+                        else if(error.status==404) {
+                            alert("Error: El dato  " + JSON.stringify(newGeneralPublicExpenses.title,null,2) + " no existe.");
+                        }
+                    });
+                };
+}]);

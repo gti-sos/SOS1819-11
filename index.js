c9d1dac862b39056dae3415d39400b2a71a13234
sos1819jmcc <jmcentenocaballero@gmail.com>
2019-04-24 02:16:54 +0000

    FE heroku v1.0

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ clientAJSM.connect(err => {
 
 //---------------------------------------Minipostman Juan Manuel Centeno -------------------------------------------
 
-app.use('/ui/v1/public-expenditure-educations', express.static(path.join(__dirname, "/public/public-expenditure-educations/views")));
+app.use('/#!/ui/v1/public-expenditure-educations/', express.static(path.join(__dirname, "/public/public-expenditure-educations/views")));
 
 
 

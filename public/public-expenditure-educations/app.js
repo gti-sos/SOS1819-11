a1e514047e9a4600a947fc7a5d6247215443a788
sos1819jmcc <jmcentenocaballero@gmail.com>
2019-04-24 01:45:53 +0000

    FE heroku v1.0

--- public/public-expenditure-educations/app.js
+++ public/public-expenditure-educations/app.js
@@ -1,5 +1,24 @@
 /*global angular*/
 
-angular.module("App", ["ngDialog", 'ngRoute']);
+angular.module("App", [ "ngRoute", "ngDialog"]).config(function($routeProvider,$locationProvider) {
+    
+    
+    /* for delete /#!*/
+    
+    
+    $locationProvider.html5Mode({
+       enabled: true,
+       requireBase: false
+    });
+    
+    
+    $routeProvider.
+    when("/ui/v1/public-expenditure-educations/:country/:year", {
+        templateUrl: "/public-expenditure-educations/views/educationsEdit.html",
+        controller: "EducatiosEditCtrl"
+       
+    });
+    
+});
 
 console.log("App initialized!");

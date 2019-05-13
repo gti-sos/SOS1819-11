2002443924ff3cb65b2d27606c42e98ab12607a9
sos1819jmcc <jmcentenocaballero@gmail.com>
2019-05-13 23:55:40 +0000

    tests

--- tests/mainTests.js
+++ tests/mainTests.js
@@ -1,19 +1,19 @@
 describe("Check if data is loaded: ",function () {
     it("List shows more than 3 items", function (){
-        browser.get("http://localhost:8080/#!/ui/v1/public-expenditure-educations");
+        browser.get("https://sos1819-11.herokuapp.com/#!/ui/v1/public-expenditure-educations");
         var getLista = element.all(by.repeater("i in getLista"));
-        expect(getLista.count()).toBeGreaterThan(0);
+        expect(getLista.count()).toBeGreaterThan(3);
 
         
     });
     
     it('should add a new data education', function() {
         browser.
-        get("http://localhost:8080/#!/ui/v1/public-expenditure-educations");
+        get("https://sos1819-11.herokuapp.com/#!/ui/v1/public-expenditure-educations");
         element.all(by.repeater('i in getLista')).then(function(getLista) {
 
-            element(by.model('data.country')).sendKeys('blablabla');
-            element(by.model('data.year')).sendKeys(2040);
+            element(by.model('data.country')).sendKeys('pekin');
+            element(by.model('data.year')).sendKeys(2010);
             element(by.model('data.educationExpense')).sendKeys(123);
             element(by.model('data.educationExpensePub')).sendKeys(123);
             element(by.model('data.educationExpensePib')).sendKeys(123);
@@ -21,12 +21,17 @@ describe("Check if data is loaded: ",function () {
             element(by.model('data.var_')).sendKeys(123);
 
             element(by.id('add')).click();
+            
+            element.all(by.repeater('i in getLista')).then(function(getListaFinal) {
+                
+                expect(getListaFinal.length).toEqual(getLista.length+1);
+            });
         });
     });
     
     it('should delete a data education', function() {
         browser.
-        get("http://localhost:8080/#!/ui/v1/public-expenditure-educations");
+        get("https://sos1819-11.herokuapp.com/#!/ui/v1/public-expenditure-educations");
         element.all(by.repeater('i in getLista')).then(function(getLista) {
             var d = element(by.model('delete(estonia,2015)'));
             element(by.id('delete')).click();

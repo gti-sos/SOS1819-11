a767f5f5af9ef8e1587332b60f8a0a6e61b847a4
sos1819jmc <joamocap@gmail.com>
2019-06-04 17:45:27 +0000

    D03

--- public/public-health-expenses/controllers/apiSOS-angular-chart.js
+++ public/public-health-expenses/controllers/apiSOS-angular-chart.js
@@ -1,43 +1,50 @@
 /* global angular */
 
-var app = angular.module("App"); //, ["chart.js"]
+var app = angular.module("App");
 
-// var data = [];
+var countries = [];
+var totalPublicExpense = [];
 
 app.controller("apiSOS-angular-chart", ["$scope", "$http", function($scope, $http) {
 
     console.log("apiSOS-angular-chart initialized!");
 
-    // $http.get("https://sos1819-11.herokuapp.com/api/v2/public-health-expenses").then(function(response) {
-    //     for (var i = 0; i < response.data.length; i++) {
-    //         data.push([response.data[i].country, response.data[i].healthExpenditurePerCapita]);
-    //     }
-
-    $scope.labels = ["January", "February", "March", "April", "May", "June", "July"];
-    $scope.series = ['Series A', 'Series B'];
-    $scope.data = [
-        [65, 59, 80, 81, 56, 55, 40],
-        [28, 48, 40, 19, 86, 27, 90]
-    ];
-    $scope.onClick = function(points, evt) {
-        console.log(points, evt);
-    };
-    $scope.datasetOverride = [{ yAxisID: 'y-axis-1' }, { yAxisID: 'y-axis-2' }];
-    $scope.options = {
-        scales: {
-            yAxes: [{
-                    id: 'y-axis-1',
-                    type: 'linear',
-                    display: true,
-                    position: 'left'
-                },
-                {
-                    id: 'y-axis-2',
-                    type: 'linear',
-                    display: true,
-                    position: 'right'
-                }
-            ]
+    $http.get("https://sos1819-11.herokuapp.com/api/v2/public-health-expenses").then(function(response) {
+        for (var i = 0; i < response.data.length; i++) {
+            countries.push(response.data[i].country);
+            totalPublicExpense.push(response.data[i].totalPublicExpense);
         }
-    };
+
+        // $scope.labels = ["January", "February", "March", "April", "May", "June", "July"];
+        $scope.labels = countries;
+        $scope.series = ['Series A', 'Series B'];
+        // $scope.data = [
+        //     [65, 59, 80, 81, 56, 55, 40],
+        //     [28, 48, 40, 19, 86, 27, 90]
+        // ];
+        $scope.data = [totalPublicExpense];
+        $scope.onClick = function(points, evt) {
+            console.log(points, evt);
+        };
+        $scope.datasetOverride = [{ yAxisID: 'y-axis-1' }, { yAxisID: 'y-axis-2' }];
+        $scope.options = {
+            scales: {
+                yAxes: [{
+                        id: 'y-axis-1',
+                        type: 'linear',
+                        display: true,
+                        position: 'left'
+                    },
+                    {
+                        id: 'y-axis-2',
+                        type: 'linear',
+                        display: true,
+                        position: 'right'
+                    }
+                ]
+            }
+        };
+
+    });
+
 }]);
